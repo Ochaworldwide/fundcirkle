a1aa33b65010d8a0665a2300c912fb312600e5f8
Ochaworldwide <lazarusgabriel000@gmail.com>
2024-11-15 17:12:56 +0100

    update

--- src/Component/Quickstats/Quickstats.jsx
+++ src/Component/Quickstats/Quickstats.jsx
@@ -31,7 +31,7 @@ const QuickStats = () => {
 
   return (
     <motion.div
-      className="w-[95%] mx-auto"
+      className="w-[95%] mx-auto mb-20"
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8, ease: "easeOut" }}
