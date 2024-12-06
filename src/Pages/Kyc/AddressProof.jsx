e3cbd3e1a55193e4032d38e76b50114422eb0f04
Ochaworldwide <lazarusgabriel000@gmail.com>
2024-12-06 19:09:02 +0100

    correction

--- src/Pages/Kyc/AddressProof.jsx
+++ src/Pages/Kyc/AddressProof.jsx
@@ -31,155 +31,39 @@ const AddressProof = () => {
           <p className="mb-5 text-[18px]">Select Address Proof Type:</p>
 
           <div className="flex items-center mb-5">
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z"
-                stroke="#141B34"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"
-                stroke="#141B34"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            <input type="radio" name="selected" id="" />
 
             <p className="text-[12px] ml-5">Electric Bill</p>
 
-            {/* <p className="rounded-md bg-white px-1 py-2 text-[12px] ml-auto">
-              Recommended
-            </p> */}
+            
           </div>
 
           <div className="flex items-center mb-5">
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z"
-                stroke="#141B34"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"
-                stroke="#141B34"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            <input type="radio" name="selected" id="" />
 
             <p className="text-[12px] ml-5">Gas Bill</p>
           </div>
 
           <div className="flex items-center mb-5">
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z"
-                stroke="#141B34"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"
-                stroke="#141B34"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            <input type="radio" name="selected" id="" />
 
             <p className="text-[12px] ml-5">Bank Account Statement</p>
           </div>
 
           <div className="flex items-center mb-5">
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z"
-                stroke="#141B34"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"
-                stroke="#141B34"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            <input type="radio" name="selected" id="" />
 
             <p className="text-[12px] ml-5">Landline Bill</p>
           </div>
 
           <div className="flex items-center mb-5">
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z"
-                stroke="#141B34"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"
-                stroke="#141B34"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            <input type="radio" name="selected" id="" />
 
             <p className="text-[12px] ml-5">Life Insurance Policy</p>
           </div>
 
           <div className="flex items-center mb-5">
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z"
-                stroke="#141B34"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"
-                stroke="#141B34"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            <input type="radio" name="selected" id="" />
 
             <p className="text-[12px] ml-5">Registered Lease/Rent Agreement</p>
           </div>
@@ -263,12 +147,10 @@ const AddressProof = () => {
         </div>
 
         <Link to="/thanks">
-            <button className="bg-[#00943F] text-[14px] rounded-md w-full text-white font-extrabold p-5">
+          <button className="bg-[#00943F] text-[14px] rounded-md w-full text-white font-extrabold p-5">
             Submit for Review
-            </button>
+          </button>
         </Link>
-
-        
       </div>
     </div>
   );
