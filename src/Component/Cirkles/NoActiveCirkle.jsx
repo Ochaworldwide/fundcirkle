d5e1faf8c4421d188c8845e759e2329d01e09016
Ochaworldwide <lazarusgabriel000@gmail.com>
2025-01-11 15:17:39 +0100

    updated my api

--- src/Component/Cirkles/NoActiveCirkle.jsx
+++ src/Component/Cirkles/NoActiveCirkle.jsx
@@ -6,18 +6,18 @@ const NoActiveCirkle = () => {
   const { openModal } = useModal();
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col items-center mt-10 justify-center bg-white border rounded-lg p-6 h-72 shadow-md w-96 mx-auto">
+    <div className="flex flex-col items-center mt-10 justify-center bg-white border rounded-lg p-6 h-72 shadow-md w-[100%] mx-auto">
       <p className="text-center text-gray-600 text-base mb-8">
         You are currently not part of any active Cirkle.
       </p>
       <button
-        className="bg-green-600 text-white text-sm font-bold py-4 px-6 rounded-lg w-full mb-5 hover:bg-green-700"
+        className="bg-green-600 text-white text-sm font-bold py-4  rounded-lg w-full mb-5 hover:bg-green-700"
         onClick={() => openModal("create")}
       >
         Create New Cirkle
       </button>
       <button
-        className="border text-green-600 text-sm font-bold py-4 px-6 rounded-lg w-full hover:bg-gray-100"
+        className="border text-green-600 text-sm font-bold py-4  rounded-lg w-full hover:bg-gray-100"
         onClick={() => navigate("/discover")}
       >
         Join a New Cirkle
