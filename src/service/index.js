2e0ebb7e7478e7955d28a3c5092bdfac595d3c08
Ochaworldwide <lazarusgabriel000@gmail.com>
2025-01-11 13:02:49 +0100

    updated my api

--- src/service/index.js
+++ src/service/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const token = localStorage.token ?? '';
+const token = localStorage.getItem('token') ?? '';
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
